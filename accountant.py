c4b9b8bd0b1c5c79dc1a61e0a6937c08788a5992
Marta Skibicka <marta.skibicka.contact@gmail.com>
2021-05-27 14:17:29 +0200

    Realizacja komend zakup, sprzedaz, stop

--- accountant.py
+++ accountant.py
@@ -1,7 +1,6 @@
 
 dane = []
 
-
 while True:
     akcja = input()
     if not akcja:
@@ -10,8 +9,25 @@ while True:
         kwota = input()
         komentarz = input()
         dane.append([akcja, kwota, komentarz])
-        print("Potwierdzam wprowadzenie: {} {}.".format(kwota, komentarz))
-        print(dane)
+        print("Potwierdzam wprowadzenie salda -> kwota: {}, komentarz: {}.".format(kwota, komentarz))
+        #print(dane)
+    elif akcja == "zakup":
+        identifykator = input()
+        cena_jedn = input()
+        ilosc_sztuk = input()
+        dane.append([identifykator, cena_jedn, ilosc_sztuk])
+        print("Potwierdzam wprowadzenie zakupu -> identyfikator: {}, cena jednostki: {}, sztuk: {}.".format(identifykator, cena_jedn, ilosc_sztuk))
+        #print(dane)
+    elif akcja == "sprzedaz":
+        identifykator = input()
+        cena_jedn = input()
+        ilosc_sztuk = input()
+        dane.append([identifykator, cena_jedn, ilosc_sztuk])
+        print("Potwierdzam wprowadzenie sprzedaży -> identyfikator: {}, cena jednostki: {}, sztuk: {}.".format(identifykator, cena_jedn, ilosc_sztuk))
+    elif akcja == "stop":
+        print("Koniec wprowadzania danych")
+        break
     else:
         print("Wprowadzono nieprawidłową komendę!")
         break
+
