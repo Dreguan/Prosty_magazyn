d20088fdecb45775158c3e6ee57e20b56959062b
Marta Skibicka <marta.skibicka.contact@gmail.com>
2021-05-31 21:35:26 +0200

    Dodanie magazynu jako słownika

--- accountant.py
+++ accountant.py
@@ -1,6 +1,6 @@
 saldo = 0
 konto = []
-magazyn = []
+magazyn = {}
 
 while True:
     print("Wprowadź komendę:")
@@ -14,14 +14,18 @@ while True:
         print("Wprowadź komentarz:")
         komentarz = str(input())
         if saldo < 0:
-            print("Saldo nie może wynosić mniej niż 0! "
-                  "Wprowadzone saldo -> kwota: {}, komentarz: {} "
-                  "<- zostaje pominięte!".format(kwota, komentarz))
+            print(
+                "Saldo nie może wynosić mniej niż 0! "
+                "Wprowadzone saldo -> kwota: {}, komentarz: {} "
+                "<- zostaje pominięte!".format(kwota, komentarz)
+            )
             continue
         else:
             konto.append([akcja, kwota, komentarz])
-            print("Potwierdzam wprowadzenie salda -> kwota: {}, komentarz: {}."
-                  .format(kwota, komentarz))
+            print(
+                "Potwierdzam wprowadzenie salda -> kwota: {}, komentarz: {}."
+                .format(kwota, komentarz)
+            )
             print("Saldo wynosi teraz: {}".format(saldo))
     elif akcja == "zakup":
         print("Wprowadź identyfikator:")
@@ -35,35 +39,33 @@ while True:
         saldo -= zakup
         print("Saldo wynosi teraz: {}".format(saldo))
         if saldo < 0:
-            print("Saldo nie może wynosić mniej niż 0! "
-                  "Wprowadzone saldo -> kwota: {}, komentarz: {} "
-                  "<- zostaje pominięte!".format(kwota, komentarz))
+            print(
+                "Saldo nie może wynosić mniej niż 0! "
+                "Wprowadzone saldo -> kwota: {}, komentarz: {} "
+                "<- zostaje pominięte!".format(kwota, komentarz)
+            )
             continue
-        elif cena_jedn < 0:
-            print("Cena jednostkowa nie może wynosić mniej niż 0! "
-                  "Wprowadzone saldo -> kwota: {}, komentarz: {} "
-                  "<- zostaje pominięte!".format(kwota, komentarz))
+        elif cena_jedn < 0 or ilosc_sztuk < 0:
+            if cena_jedn < 0:
+                bledne_pole = "cena jednostkowa"
+            elif ilosc_sztuk < 0:
+                bledne_pole = "ilość sztuk"
+            print(
+                "{} nie może wynosić mniej niż 0! "
+                "Wprowadzone saldo -> kwota: {}, komentarz: {} "
+                "<- zostaje pominięte!".format(bledne_pole, kwota, komentarz)
+            )
             continue
-        elif ilosc_sztuk < 0:
-            print("Ilość sztuk nie może wynosić mniej niż 0! "
-                  "Wprowadzone saldo -> kwota: {}, komentarz: {} "
-                  "<- zostaje pominięte!".format(kwota, komentarz))
-            continue
-        elif identifykator == "raspberry":
-            if not magazyn:
-                magazyn.append([identifykator, cena_jedn, ilosc_sztuk])
-                print("Potwierdzam wprowadzenie zakupu -> identyfikator: {}, "
-                      "cena jednostki: {}, sztuk: {}."
-                      .format(identifykator, cena_jedn, ilosc_sztuk))
-                #print("...ILOŚĆ RASPBERRY TERAZ: {}".format(ilosc_sztuk))
-            else:
-                magazyn[0][2] += ilosc_sztuk
-                print("Potwierdzam wprowadzenie zakupu -> identyfikator: {}, "
-                      "cena jednostki: {}, sztuk: {}."
-                      .format(identifykator, cena_jedn, ilosc_sztuk))
-                #print("ILOŚĆ RASPBERRY TERAZ: {}".format(magazyn[0][2]))
-            print("Saldo wynosi teraz: {}".format(saldo))
-        konto.append([akcja, identifykator, cena_jedn, ilosc_sztuk])
+        else:
+            konto.append([akcja, identifykator, cena_jedn, ilosc_sztuk])
+            print(
+                "Potwierdzam wprowadzenie zakupu -> identyfikator: {}, "
+                "cena jednostki: {}, sztuk: {}."
+                    .format(identifykator, cena_jedn, ilosc_sztuk)
+            )
+            if identifykator not in magazyn:
+                magazyn[identifykator] = 0
+            magazyn[identifykator] += ilosc_sztuk
     elif akcja == "sprzedaz":
         print("Wprowadź identyfikator:")
         identifykator = str(input())
@@ -74,46 +76,45 @@ while True:
         sprzedaz = cena_jedn * ilosc_sztuk
         #print(sprzedaz)
         saldo += sprzedaz
-        if saldo < 0:
-            print("Saldo nie może wynosić mniej niż 0! "
-                  "Wprowadzone saldo -> kwota: {}, komentarz: {} "
-                  "<- zostaje pominięte!".format(kwota, komentarz))
+        if cena_jedn < 0 or ilosc_sztuk < 0:
+            if cena_jedn < 0:
+                bledne_pole = "cena jednostkowa"
+            elif ilosc_sztuk < 0:
+                bledne_pole = "ilość sztuk"
+            print(
+                "{} nie może wynosić mniej niż 0! "
+                "Wprowadzone saldo -> kwota: {}, komentarz: {} "
+                "<- zostaje pominięte!".format(bledne_pole, kwota, komentarz)
+            )
             continue
-        elif identifykator == "raspberry":
-            magazyn[0][2] -= ilosc_sztuk
-            print("Potwierdzam wprowadzenie sprzedaży -> identyfikator: {},"
-                  "cena jednostki: {}, sztuk: {}."
-                  .format(identifykator, cena_jedn, ilosc_sztuk))
-            print("Saldo wynosi teraz: {}".format(saldo))
-            #print("ILOŚĆ RASPBERRY TERAZ: {}".format(magazyn[0][2]))
         else:
-            magazyn.append([identifykator, cena_jedn, ilosc_sztuk])
-            print("Potwierdzam wprowadzenie sprzedaży -> identyfikator: {},"
-                  "cena jednostki: {}, sztuk: {}."
-                  .format(identifykator, cena_jedn, ilosc_sztuk))
-            print("Saldo wynosi teraz: {}".format(saldo))
-        konto.append([akcja, identifykator, cena_jedn, ilosc_sztuk])
-    elif akcja == "konto":
-        print("\nHistoria konta:")
-        for element in konto:
-            print(element)
-        break
-    elif akcja == "magazyn":
-        print("\nStan magazynu:")
-        for element in magazyn:
-            print(magazyn[0][0], magazyn[0][2])
-        break
+            konto.append([akcja, identifykator, cena_jedn, ilosc_sztuk])
+            if identifykator not in magazyn:
+                print("Nie ma takiego produktu w magazynie!")
+            print(
+                "Potwierdzam wprowadzenie sprzedaży -> identyfikator: {}, "
+                "cena jednostki: {}, sztuk: {}."
+                    .format(identifykator, cena_jedn, ilosc_sztuk)
+            )
+            magazyn[identifykator] -= ilosc_sztuk
     elif akcja == "stop":
         print("Koniec wprowadzania danych")
         break
     else:
         print("Wprowadzono nieprawidłową komendę!")
         break
+'''
+    elif akcja == "przeglad":
+        print("\nHistoria konta:")
+        for wpis in konto:
+            for element in wpis:
+                print(element)
+        break
+    elif akcja == "konto":
+        print(saldo)
+        break
+'''
 
-#print("\nHistoria konta:")
-#for element in konto:
-#    print(element)
-
-#print("\nStan magazynu:")
-#for element in magazyn:
-#    print(magazyn)
+print("\n {}".format(saldo))
+print("\n {}".format(konto))
+print("\n {}".format(magazyn))
