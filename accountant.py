c100e6401d098fe2d8c1afb0ba404e48e7712ca2
Marta Skibicka <marta.skibicka.contact@gmail.com>
2021-06-07 21:47:04 +0200

    Ulepszone działanie komend zakup i sprzedaz z konsoli

--- accountant.py
+++ accountant.py
@@ -21,12 +21,6 @@ while True:
             continue
         else:
             log.append([akcja, kwota, komentarz])
-            '''
-            print(
-                "Potwierdzam wprowadzenie salda -> kwota: {}, komentarz: {}."
-                .format(kwota, komentarz)
-            )
-            '''
         continue
     if akcja == "zakup":
         identifykator = str(input())
@@ -54,13 +48,6 @@ while True:
             continue
         else:
             log.append([akcja, identifykator, cena_jedn, ilosc_sztuk])
-            '''
-            print(
-                "Potwierdzam wprowadzenie zakupu -> identyfikator: {}, "
-                "cena jednostki: {}, sztuk: {}."
-                    .format(identifykator, cena_jedn, ilosc_sztuk)
-            )
-            '''
             if identifykator not in magazyn:
                 magazyn[identifykator] = 0
             magazyn[identifykator] += ilosc_sztuk
@@ -86,17 +73,11 @@ while True:
             log.append([akcja, identifykator, cena_jedn, ilosc_sztuk])
             if identifykator not in magazyn:
                 print("Nie ma takiego produktu w magazynie!")
-            '''
-            print(
-                "Potwierdzam wprowadzenie sprzedaży -> identyfikator: {}, "
-                "cena jednostki: {}, sztuk: {}."
-                    .format(identifykator, cena_jedn, ilosc_sztuk)
-            )
-            '''
             magazyn[identifykator] -= ilosc_sztuk
         continue
 
     if sys.argv[1] == "zakup":
+        akcja = sys.argv[1]
         identifykator = str(sys.argv[2])
         cena_jedn = int(sys.argv[3])
         ilosc_sztuk = int(sys.argv[4])
@@ -122,16 +103,37 @@ while True:
             continue
         else:
             log.append([akcja, identifykator, cena_jedn, ilosc_sztuk])
-            '''
-            print(
-                "Potwierdzam wprowadzenie zakupu -> identyfikator: {}, "
-                "cena jednostki: {}, sztuk: {}."
-                    .format(identifykator, cena_jedn, ilosc_sztuk)
-            )
-            '''
             if identifykator not in magazyn:
                 magazyn[identifykator] = 0
             magazyn[identifykator] += ilosc_sztuk
+        for wpis in log:
+            for element in wpis:
+                print(element)
+        continue
+
+    if sys.argv[1] == "sprzedaz":
+        akcja = sys.argv[1]
+        identifykator = str(sys.argv[2])
+        cena_jedn = int(sys.argv[3])
+        ilosc_sztuk = int(sys.argv[4])
+        sprzedaz = cena_jedn * ilosc_sztuk
+        saldo += sprzedaz
+        if cena_jedn < 0 or ilosc_sztuk < 0:
+            if cena_jedn < 0:
+                bledne_pole = "cena jednostkowa"
+            elif ilosc_sztuk < 0:
+                bledne_pole = "ilość sztuk"
+            print(
+                "{} nie może wynosić mniej niż 0! "
+                "Wprowadzone saldo -> kwota: {}, komentarz: {} "
+                "<- zostaje pominięte!".format(bledne_pole, kwota, komentarz)
+            )
+            continue
+        else:
+            log.append([akcja, identifykator, cena_jedn, ilosc_sztuk])
+            if identifykator not in magazyn:
+                print("Nie ma takiego produktu w magazynie!")
+            magazyn[identifykator] -= ilosc_sztuk
         for wpis in log:
             for element in wpis:
                 print(element)
@@ -158,18 +160,9 @@ while True:
         break
 
     if akcja == "stop":
-        print("Koniec wprowadzania danych")
         for wpis in log:
             for element in wpis:
                 print(element)
-        break
 
     else:
         print("Wprowadzono nieprawidłową komendę!")
-        break
-
-
-
-# print("\n {}".format(saldo))
-# print("\n {}".format(log))
-# print("\n {}".format(magazyn))
